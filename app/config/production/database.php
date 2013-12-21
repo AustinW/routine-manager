ee765803f9242536ba71d5a73c7a96bda2d8f220
Austin White <austingym@gmail.com>
2013-12-20 22:45:08 -0800

    Production settings

---
+++ app/config/production/database.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+
+	'default' => 'mongodb',
+
+	'connections' => array(
+
+		'mongodb' => array(
+			'driver'   => 'mongodb',
+			'host'     => 'ds061198.mongolab.com',
+			'port'     => 61198,
+			'username' => 'austinw',
+			'password' => '8CpKKrTdJbGbfM3A',
+			'database' => 'routine_manager'
+		),
+	)
+);
