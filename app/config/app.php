8571fb285a963ce213bcd05aab641263735d9fe7
Austin White <austingym@gmail.com>
2013-12-24 16:27:30 -0800

    Trying to use Mongo instead of MongoClient

--- app/config/app.php
+++ app/config/app.php
@@ -175,6 +175,8 @@ return array(
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
 
+		'MongoClient'     => 'Mongo',
+
 	),
 
 );
